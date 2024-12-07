33d74fb3ab2d360f0cffc9957b06844114bf48a4
Victor Kenzo <161893973+2StaiNN2@users.noreply.github.com>
2024-12-07 10:42:31 -0300

    cria metodo formata_data, e atualiza movimentacao

--- Contas.py
+++ Contas.py
@@ -5,29 +5,43 @@ class Conta:
         self.saldo = 0.0
         self.movimentacoes = []
 
+    def _formata_data(self, data_str):
+        dia, mes, ano = map(int, data_str.split('/'))
+        data_formatada = (ano, mes, dia)
+        return data_formatada
+
     def movimentacao(self, valor, data, descricao):
-        if (valor < 0) and (self.saldo + valor < 0):
-            return False, "Saldo da conta insuficiente."
-        
-        if (self.movimentacoes and data) < (self.movimentacoes[-1]["data"]):
-            return False, "Movimentações não podem ser feitas em datas retroativas."
+        saque = valor < 0
+
+        if valor == 0:
+            return False, "Não e possível realizar movimentação de R$ 0,00"
+
+        if saque and (self.saldo + valor < 0):
+            return False, "Saldo insuficiente"
+
+        if self.movimentacoes and data < self.movimentacoes[-1]["data"]:
+            return False, "Movimentação tem data retroativa"
 
         self.saldo += valor
         self.movimentacoes.append({
             "valor": valor,
-            "data": data,
+            "data": self._formata_data(data),
             "descricao": descricao,
             "saldo_apos": self.saldo
         })
-        return True, "Movimentação bem sucedida!"
-    
+
+        if saque:
+            return True, "Saque realizado com sucesso"
+
+        return True, "Depósito realizado com sucesso"
+
 class Banco:
     def __init__(self):
         self.Conta = {}
 
     def criar_conta(self, numero):
         if numero in self.contas:
-            return False, "Número de conta já existe."
+            return False, "Número de conta já existe"
         
         self.contas[numero] = ContaBancaria(numero)
         return True, "Conta aberta com sucesso!"
@@ -36,4 +50,4 @@ class Banco:
         if numero not in self.contas:
             return None, "Esta conta não existe."
         
-        return self.contas[numero].saldo, "Consulta de saldo realizada!"
+        return "O saldo da conta é R$ " , self.contas[numero].saldo
